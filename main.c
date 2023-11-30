bf87e6318d04114e457286d4ef40325393a07f6a
eun1337 <dbdmstj0327@naver.com>
2023-11-30 15:28:21 +0900

    02

--- main.c
+++ main.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <math.h>
 
-struct student {
-	int ID; // 학번 
-	char name[20]; // 이름 
-	double grade; // 학점 
+struct point {
+	int x;
+	int y; 
 };
 
 int main(void) 
 {
-	struct student s1 = {2314513, "Eun Seo", 4.3};
+	struct point p1, p2;
+	int xdiff, ydiff;
+	double dist;
 	
-	s1.ID = 1234567;
-	s1.name[0] = 'C';
-	s1.grade = 4.2;
+	printf("input p1  coordinate (x y) :");
+	scanf("%d %d", &p1.x, &p1.y);
 	
-	printf("ID : %d\n", s1.ID);
-	printf("name : %s\n", s1.name);
-	printf("grade : %f\n", s1.grade);
+	printf("input p2  coordinate (x y) :");
+	scanf("%d %d", &p2.x, &p2.y);
+	
+	xdiff = p2.x - p1.x;
+	ydiff = p2.y - p1.y;
+	dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+	
+	printf("distance : %f\n", dist);
 
 	return 0;
 }
